6337a3a947f9f05f13f25c399b38fa32f5d809d9
Elizabeth Rodricks <elizabethrodricks@Elizabeths-iMac.local>
2023-07-13 08:39:45 -0400

    still trying to get label short on filter working

--- src/pages/LandingPage/EmbedForm/ProductMovement/helpers/CurrentSelection.js
+++ src/pages/LandingPage/EmbedForm/ProductMovement/helpers/CurrentSelection.js
@@ -4,49 +4,51 @@ import { Button, Form, Modal, Spinner, Row, Col } from "react-bootstrap";
 export const CurrentSelection = ({selectedDateFilter, selectedFilters, setSelectedFilters, filterOptions, fieldOptions, selectedFields, setSelectedFields, dateFilterOptions}) => {
     const [currentSelection, setCurrentSelection] = useState([])
 
-
-
-    console.log("these are filters", selectedFilters)
     console.log("filter options", filterOptions)
+    console.log(dateFilterOptions)
+
+
 
     useEffect(() => {
         let currentSelectionObj = {};
         if (selectedDateFilter !== "") {
 
-          // const option3 = dateFilterOptions.find(option3 => option3.name === selectedDateFilter[filter]);
-          //
-          // if(option3){
-          //   currentSelectionObj[filter] = option3;
-          // }
+          const option3 = dateFilterOptions.find(option3 => option3.name === selectedDateFilter);
 
-            currentSelectionObj[selectedDateFilter] = 'Yes'
+          if(option3){
+            currentSelectionObj[selectedDateFilter] = option3;
+          }
+
+            // currentSelectionObj[selectedDateFilter] = 'Yes'
         }
 
-        for (const filter in selectedFields) {
-            if (selectedFields[filter] !== "") {
-              const option1 = fieldOptions.find(option1 => option1.name === selectedFields[filter]);
-
-              if(option1){
-                currentSelectionObj[filter] = option1;
-              }
-                // currentSelectionObj[filter] = selectedFields[filter];
-            }
-        }
-
+        // for (const filter in selectedFields) {
+        //     if (selectedFields[filter] !== "") {
+        //       const option1 = fieldOptions.find(option1 => option1.name === selectedFields[filter]);
+        //
+        //       if(option1){
+        //         currentSelectionObj[filter] = option1;
+        //       }
+        //         // currentSelectionObj[filter] = selectedFields[filter];
+        //     }
+        //
+        //         // console.log("one", selectedFields)
+        // }
+        //
 
 
         for (const filter in selectedFilters) {
             if (selectedFilters[filter] && selectedFilters[filter] !== "N/A") {
-              const option2 = filterOptions.find(optionFilter => option2.name === selectedFilters[filter]);
+              const option2 = filterOptions.find(option2 => option2.name === selectedFilters[filter]);
 
               if(option2){
                 currentSelectionObj[filter] = option2;
               }
 
-
-
                 // currentSelectionObj[filter] = selectedFilters[filter];
             }
+
+            // console.log("two", selectedFilters)
         }
 
       setCurrentSelection(currentSelectionObj)
@@ -54,7 +56,7 @@ export const CurrentSelection = ({selectedDateFilter, selectedFilters, setSelect
 
 
     function removeField(fieldName) {
-      setSelectedFields((prev) => {
+      setSelectedDateFilter((prev) => {
         if (prev.includes(fieldName)) {
           return prev.filter((selectedFilter) => selectedFilter !== fieldName);
         } else {
@@ -81,7 +83,7 @@ export const CurrentSelection = ({selectedDateFilter, selectedFilters, setSelect
                   {/*<p className="mb-0">{currentSelection[selection]}</p>*/}
 
 
-                    <p className="mb-0">{currentSelection[selection].label_short}</p>
+                    <p className="mb-0">{currentSelection[selection].label_short.replace(/\s*\(.*?\)\s*/g, '')}</p>
 
                     <i onClick={() => removeField(currentSelection[selection].name)} class="fal fa-trash-undo red"></i>
 
