f9d2c1b7b28995188813db458345532f72318f69
elizabethrodricks <114446653+elizabethrodricks@users.noreply.github.com>
2023-08-22 13:18:17 -0400

    Update constants.js
    updated main constants back to normal

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,43 +1,18 @@
-export const LOOKER_MODEL = "order_express";
-
-export const LOOKER_EXPLORE = "time_detail_cv";
-
- 
-
- 
-
- 
+export const LOOKER_MODEL = "rebecca_thompson_project";
+export const LOOKER_EXPLORE = "sdt_all_dates";
 
 export const PRODUCT_MOVEMENT_VIS_DASHBOARD_ID =
-
   // "rebecca_thompson_project::product_movement_details";
-
-  // "rebecca_thompson_project::product_movement_report_tab";
-
-  "order_express::product_movement_report_tab";
-
- 
-
- 
-
- 
+  "rebecca_thompson_project::product_movement_report_tab";
 
 export const LOOKML_FIELD_TAGS = {
-
   filter: "filter: product_movement",
-
   date_filter: "date filter",
-
   productMovementField: "field: product_movement",
-
   dateRange: "date range",
-
   quick_filter: "quick_filter: product_movement",
-
   totalInvoices: "total_invoices",
-
-  // accountGroups: "account_group",
-
-  togglePrefix: "toggle: "
-
+  accountGroups: "account_group",
+  togglePrefix: "toggle: ",
+  defaultFilters: "default_filter: purchase_review"
 };
