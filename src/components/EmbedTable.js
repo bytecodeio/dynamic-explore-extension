5f75cefaf52800aa70db87457e62059a6d5c633c
elizabethrodricks <114446653+elizabethrodricks@users.noreply.github.com>
2023-08-22 13:20:15 -0400

    Update EmbedTable.js
      `${hostUrl}/embed/query/rebecca_thompson_project/order_items?qid=${queryId}&sdk=2&embed_domain=${hostUrl}&sandboxed_host=true`

--- src/components/EmbedTable.js
+++ src/components/EmbedTable.js
@@ -32,7 +32,7 @@ const EmbedTable = ({ queryId }) => {
         el.innerHTML = "";
         LookerEmbedSDK.init(hostUrl);
         LookerEmbedSDK.createExploreWithUrl(
-            `${hostUrl}/embed/query/order_express/time_detail_cv?qid=${queryId}&sdk=2&embed_domain=${hostUrl}&sandboxed_host=true`
+            `${hostUrl}/embed/query/rebecca_thompson_project/order_items?qid=${queryId}&sdk=2&embed_domain=${hostUrl}&sandboxed_host=true`
           )
           .appendTo(el)
           .build()
