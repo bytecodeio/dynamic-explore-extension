bc63faea6939d004ed6d2e756221a930afd5120e
aaronmodic <80269391+aaronmodic@users.noreply.github.com>
2023-10-09 17:43:33 +0000

    Added Application Context and modified field code to include subtabs

--- src/LayoutSelector.js
+++ src/LayoutSelector.js
@@ -12,24 +12,10 @@ import { useRouteMatch } from "react-router-dom/cjs/react-router-dom";
 export const LayoutSelector = ({
     tabProps,
     currentNavTab,
-    filters,
     fields,
     properties,
-    parameters,
-    updateAppProperties,
-    isFetchingLookmlFields,
-    showMenu,
-    setShowMenu,
-    keyword,
-    setKeyword,
-    handleChangeKeyword,
-    selectedFilters,
-    setSelectedFilters,
-    updatedFilters,
-    setUpdatedFilters,
-    initialLoad,
-    setInitialLoad,
-    isActive, application, tabFilters, savedFilters, removeSavedFilter, upsertSavedFilter}) => {
+    isActive, 
+    tabFilters}) => {
         console.log("props", properties)
         console.log("Tabprops", tabProps)
     if (tabProps.layout_name === "Template1") {
@@ -37,86 +23,36 @@ export const LayoutSelector = ({
             className="active"
             isActive={isActive}
             currentNavTab={currentNavTab}
-            filters={filters}
-            fields={fields.find(({tab}) => tab === tabProps.title)}
+            fields={fields.filter(({tab}) => tab === tabProps.title)}
             properties={properties.filter(({type}) => type == tabProps.properties[0]?.type)}
-            parameters={parameters}
-            updateAppProperties={updateAppProperties}
-            isFetchingLookmlFields={isFetchingLookmlFields}
             config={tabProps.config}
             tabKey={tabProps.route}
-            showMenu={showMenu}
-            setShowMenu={setShowMenu}
             description={{description: <div dangerouslySetInnerHTML={{__html:tabProps.description}} />}}
-            selectedFilters={selectedFilters}
-            setSelectedFilters={setSelectedFilters}
-            updatedFilters={updatedFilters}
-            setUpdatedFilters={setUpdatedFilters}
-            initialLoad={initialLoad}
-            setInitialLoad={setInitialLoad}
-            keyword={keyword}
-            handleChangeKeyword={handleChangeKeyword}
-            application={application}
             tabFilters={tabFilters.filter(({tab}) => tab === tabProps.title)}
-            savedFilters={savedFilters}
-            removeSavedFilter={removeSavedFilter}
-            upsertSavedFilter={upsertSavedFilter}
             />;
     } else if (tabProps.layout_name === "Template3") {
         return <Template3
             className="active"
             isActive={isActive}
             currentNavTab={currentNavTab}
-            filters={filters}
             fields={fields.find(({tab}) => tab === tabProps.title)}
             properties={properties.filter(({type}) => type == tabProps.properties[0]?.type)}
-            parameters={parameters}
-            updateAppProperties={updateAppProperties}
-            isFetchingLookmlFields={isFetchingLookmlFields}
             config={tabProps.config}
             tabKey={tabProps.route}
-            showMenu={showMenu}
-            setShowMenu={setShowMenu}
             description={{description: <div dangerouslySetInnerHTML={{__html:tabProps.description}} />}}
-            selectedFilters={selectedFilters}
-            setSelectedFilters={setSelectedFilters}
-            updatedFilters={updatedFilters}
-            setUpdatedFilters={setUpdatedFilters}
-            initialLoad={initialLoad}
-            setInitialLoad={setInitialLoad}
-            keyword={keyword}
-            handleChangeKeyword={handleChangeKeyword}
-            application={application}
             tabFilters={tabFilters.filter(({tab}) => tab === tabProps.title)}
-            savedFilters={savedFilters}
             />;
     } else if (tabProps.layout_name === "Template4") {
         return <Template4
             className="active"
             isActive={isActive}
             currentNavTab={currentNavTab}
-            filters={filters}
             fields={fields.find(({tab}) => tab === tabProps.title)}
             properties={properties.filter(({type}) => type == tabProps.properties[0]?.type)}
-            parameters={parameters}
-            updateAppProperties={updateAppProperties}
-            isFetchingLookmlFields={isFetchingLookmlFields}
             config={tabProps.config}
             tabKey={tabProps.route}
-            showMenu={showMenu}
-            setShowMenu={setShowMenu}
             description={{description: <div dangerouslySetInnerHTML={{__html:tabProps.description}} />}}
-            selectedFilters={selectedFilters}
-            setSelectedFilters={setSelectedFilters}
-            updatedFilters={updatedFilters}
-            setUpdatedFilters={setUpdatedFilters}
-            initialLoad={initialLoad}
-            setInitialLoad={setInitialLoad}
-            keyword={keyword}
-            handleChangeKeyword={handleChangeKeyword}
-            application={application}
             tabFilters={tabFilters.filter(({tab}) => tab === tabProps.title)}
-            savedFilters={savedFilters}
             />;
     }
     // switch (tabProps.layout_name) {
